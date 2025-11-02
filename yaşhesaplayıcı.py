7f17b4fc89f1e93ed0cb36ce6d2963c5e1969678
Nehir <nehirtosun238@gmail.com>
2025-11-02 19:26:33 +0300

    Add files via upload

---
+++ yaşhesaplayıcı.py
@@ -0,0 +1,6 @@
+print("Yaş hesaplayıcıya hoşgeldiniz.")
+
+yıl = int(input("Lütfen doğum yılını giriniz:"))
+yaş = 2025 - yıl
+
+print(yaş)
---
+++ çarpımtablosu.py
@@ -0,0 +1,4 @@
+print("Çarpım tablosu geliyor...")
+for i in range (1,11):
+    for j in range (1,11):
+        print(f"{i} * {j} = {i*j}")
